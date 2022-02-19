fe7ef4941f4fb8ac1f33c179312fc8ffac81258e
oliveravictor <ovictoriox@gmail.com>
2022-02-19 00:28:49 -0300

    add Cardnews in home

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { Cardnews } from "./Cardnews/Cardnews";
 
 export function Home() {
     return(
+        <>
         <h1>Aca va a estar el Home</h1>
+        <Cardnews />    
+        </>
     )
 }
