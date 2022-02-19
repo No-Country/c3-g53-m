7f75f8c6be8fa80c38d82569b401e3219ed953b0
oliveravictor <ovictoriox@gmail.com>
2022-02-19 01:55:51 -0300

    position board

---
+++ src/components/Board/Board.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import "./Board.css"
+
+export function Board() {
+  return (
+    <div className="wrapper">
+    <h3>Temporada 2022-2023</h3>
+    <h3>Posiciones</h3>
+     <div className="row title">
+        <ul>
+        <p>Nº</p>
+          <p>Equipo</p>
+          <p>Jugados</p>
+          <p>Ganados</p>
+          <p>Perdidos</p>
+          <p>+/-</p>
+        </ul>
+      </div>
+
+      <section className="row-wrapper">
+        <div className="row nfl">
+          <ul>
+            <li>1</li>
+            <li>LNB</li>
+            <li>53</li>
+            <li>45</li>
+            <li>100</li>
+          </ul>
+        </div>
+      </section>
+
+      <section className="row-wrapper">
+        <div className="row nfl">
+          <ul>
+            <li>1</li>
+            <li>LNB</li>
+            <li>53</li>
+            <li>45</li>
+            <li>100</li>
+          </ul>
+        </div>
+      </section>
+
+      <section className="row-wrapper">
+        <div className="row nfl">
+          <ul>
+            <li>1</li>
+            <li>LNB</li>
+            <li>53</li>
+            <li>45</li>
+            <li>100</li>
+          </ul>
+        </div>
+      </section>
+
+      <section className="row-wrapper">
+        <div className="row nfl">
+          <ul>
+            <li>1</li>
+            <li>LNB</li>
+            <li>53</li>
+            <li>45</li>
+            <li>100</li>
+          </ul>
+        </div>
+      </section>
+
+      <section className="row-wrapper">
+        <div className="row nfl">
+          <ul>
+            <li>1</li>
+            <li>LNB</li>
+            <li>53</li>
+            <li>45</li>
+            <li>100</li>
+          </ul>
+        </div>
+      </section>
+   </div>
+  )
+}
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { Board } from "./Board/Board";
 import { Cardnews } from "./Cardnews/Cardnews";
 
 export function Home() {
     return(
         <>
         <h1>Aca va a estar el Home</h1>
-        <Cardnews />    
+        <Board />
+        <Cardnews />   
         </>
     )
 }
