5156db88244abfad02e4b0d305b94781d0b33902
agustin vilarino <lic.avilarino@gmail.com>
2022-02-21 09:08:35 -0300

    cards-skill-home

--- src/components/cardsSkills/cardsSkills.js
+++ src/components/cardsSkills/cardsSkills.js
@@ -1,4 +1,4 @@
-import "./cardsskills.css"
+import "./cardsskills.css";
 
 import Ginobili from "./images/ginobilicara.jpg"
 import Bryant from "./images/bryant.jpg"
@@ -8,166 +8,168 @@ import Magic from "./images/magic.jpg"
 export function Cardsplayer() {
   return (
    
-     <>
   <div class="container-cards-skills">
-  <div class="container-card">
-    <div class="card-title">
-      <div class="number">
-          <p class="title-number">11</p>
-        <div class="line-number"></div>
-      </div>
-      <div class="card-title-text">
-        <p class="color-orange">Manu</p>
-        <p>Ginobili</p>
-      </div>
-    </div>
-    <div class="card-image">
-      <img src={Ginobili} alt="nofoto" />
-    </div>
-    <div class="skills">
-      <div class="skills-flex">
-        <p class="color-grey">Altura</p>
-        <p class="color-white">1,98cm</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Posición</p>
-        <p class="color-white">Guard</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Peso</p>
-        <p class="color-white">93kg</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Last Team</p>
-        <p class="color-white">San Antonio</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Nacionalidad</p>
-        <p class="color-white">Argentina</p>
-      </div>
-    </div>
-   </div>
 
-   <div class="container-card">
-    <div class="card-title">
-      <div class="number">
-        <p class="title-number">45</p>
-        <div class="line-number"></div>
+    <div class="container-card">
+        <div class="card-title">
+          <div class="number">
+              <p class="title-number">11</p>
+              <div class="line-number"></div>
+          </div>
+          <div class="player-name">
+            <p class="orange-name">Manu</p>
+            <p class="surname">Ginobili</p>
+          </div>
+        </div>
+          <img class="foto-player" src={Ginobili} alt="no-foto" />
+          <div class="skills">
+            <div class="skills-flex">
+              <p class="color-grey">Altura</p>
+              <p class="color-white">1,98cm</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Posición</p>
+              <p class="color-white">Guard</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Peso</p>
+              <p class="color-white">96kg</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Last Team</p>
+              <p class="color-white">San Antonio</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Nacionalidad</p>
+              <p class="color-white">Argentina</p>
+            </div>
+          </div>
       </div>
-      <div class="card-title-text">
-        <p class="color-orange">Michael</p>
-        <p>Jordan</p>
-      </div>
-    </div>
-    <div class="card-image">
-      <img src={Jordan} alt="nofoto" />
-    </div>
-    <div class="skills">
-      <div class="skills-flex">
-        <p class="color-grey">Altura</p>
-        <p class="color-white">1,98cm</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Posición</p>
-        <p class="color-white">Guard</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Peso</p>
-        <p class="color-white">93kg</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Last Team</p>
-        <p class="color-white">Chicago Bulls</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Nacionalidad</p>
-        <p class="color-white">USA</p>
-      </div>
-    </div>
-   </div>
 
-   <div class="container-card">
-    <div class="card-title">
-      <div class="number">
-        <p class="title-number">11</p>
-        <div class="line-number"></div>
+      
+    <div class="container-card">
+        <div class="card-title">
+          <div class="number">
+              <p class="title-number">11</p>
+              <div class="line-number"></div>
+          </div>
+          <div class="player-name">
+            <p class="orange-name">Kobe</p>
+            <p class="surname">Bryant</p>
+          </div>
+        </div>
+          <img class="foto-player" src={Bryant} alt="no-foto" />
+          <div class="skills">
+            <div class="skills-flex">
+              <p class="color-grey">Altura</p>
+              <p class="color-white">1,98cm</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Posición</p>
+              <p class="color-white">Guard</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Peso</p>
+              <p class="color-white">96kg</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Last Team</p>
+              <p class="color-white">Lakers</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Nacionalidad</p>
+              <p class="color-white">USA</p>
+            </div>
+          </div>
       </div>
-      <div class="card-title-text">
-        <p class="color-orange">Kobe</p>
-        <p>Bryant</p>
-      </div>
-    </div>
-    <div class="card-image">
-      <img src={Bryant} alt="nofoto" />
-    </div>
-    <div class="skills">
-      <div class="skills-flex">
-        <p class="color-grey">Altura</p>
-        <p class="color-white">1,98cm</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Posición</p>
-        <p class="color-white">Guard</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Peso</p>
-        <p class="color-white">96kg</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Last Team</p>
-        <p class="color-white">Lakers</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Nacionalidad</p>
-        <p class="color-white">USA</p>
-      </div>
-    </div>
-   </div>
 
-   <div class="container-card">
-    <div class="card-title">
-      <div class="number">
-        <p class="title-number">32</p>
-        <div class="line-number"></div>
+        
+    <div class="container-card">
+        <div class="card-title">
+          <div class="number">
+              <p class="title-number">11</p>
+              <div class="line-number"></div>
+          </div>
+          <div class="player-name">
+            <p class="orange-name">Michael</p>
+            <p class="surname">Jordan</p>
+          </div>
+        </div>
+          <img class="foto-player" src={Jordan} alt="no-foto" />
+          <div class="skills">
+            <div class="skills-flex">
+              <p class="color-grey">Altura</p>
+              <p class="color-white">1,98cm</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Posición</p>
+              <p class="color-white">Guard</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Peso</p>
+              <p class="color-white">96kg</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Last Team</p>
+              <p class="color-white">Chicago Bulls</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Nacionalidad</p>
+              <p class="color-white">USA</p>
+            </div>
+          </div>
       </div>
-      <div class="card-title-text">
-        <p class="color-orange">Magic</p>
-        <p>Johnson</p>
+
+        
+    <div class="container-card">
+        <div class="card-title">
+          <div class="number">
+              <p class="title-number">11</p>
+              <div class="line-number"></div>
+          </div>
+          <div class="player-name">
+            <p class="orange-name">Magic</p>
+            <p class="surname">Johnson</p>
+          </div>
+        </div>
+          <img class="foto-player" src={Magic} alt="no-foto" />
+          <div class="skills">
+            <div class="skills-flex">
+              <p class="color-grey">Altura</p>
+              <p class="color-white">2,06cm</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Posición</p>
+              <p class="color-white">Guard</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Peso</p>
+              <p class="color-white">96kg</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Last Team</p>
+              <p class="color-white">Lakers</p>
+            </div>
+            <div class="skills-flex">
+              <p class="color-grey">Nacionalidad</p>
+              <p class="color-white">USA</p>
+            </div>
+          </div>
       </div>
-     
-    </div>
-    <div class="card-image">
-      <img src={Magic} alt="nofoto" />
-    </div>
-    <div class="skills">
-      <div class="skills-flex">
-        <p class="color-grey">Altura</p>
-        <p class="color-white">2,06cm</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Posición</p>
-        <p class="color-white">Guard</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Peso</p>
-        <p class="color-white">116kg</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Last Team</p>
-        <p class="color-white">Lakers</p>
-      </div>
-      <div class="skills-flex">
-        <p class="color-grey">Nacionalidad</p>
-        <p class="color-white">USA</p>
-      </div>
-    </div>
-   </div>
+        
 
 
-</div>
-</>
-   
+
+
+  </div>
+      
+
+
+
     
+    
+     
+        
   );
 };
 
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Board } from "./Board/Board";
 import { Cardnews } from "./Cardnews/Cardnews";
 import {Nav} from "./Nav/Nav"
+import {Cardsplayer} from "./cardsSkills/cardsSkills"
 
 
 
@@ -11,9 +12,9 @@ export function Home() {
         <>
         <Nav />
         <h1>Aca va a estar el Home</h1>
-       
         <Board />
         <Cardnews />
+        <Cardsplayer />
         
       
         </>
