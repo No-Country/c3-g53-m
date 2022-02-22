cb04862c3acf10ad838e9f0529f51accd6895163
Nicolas Orecchia <nicoaleore@gmail.com>
2022-02-22 15:39:23 -0300

    background-color

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { Board } from "./Board/Board";
 import { Cardnews } from "./Cardnews/Cardnews";
-import {Nav} from "./Nav/Nav"
+import {Nav} from "./Nav/nav"
 import {Cardsplayer} from "./cardsSkills/cardsSkills"
+import "./home.css"
 
 
 
 export function Home() {
     return(
 
-        <>
-        <Nav />
-        <h1>Aca va a estar el Home</h1>
-        <Board />
-        <Cardnews />
-        <Cardsplayer />
-        
-      
-        </>
+        <div className="caja">
+            <Nav />
+            <h1 className="prueba">Aca va a estar el Home</h1>
+            <Board />
+            <Cardnews />
+            <Cardsplayer />      
+        </div>
     )
 }
