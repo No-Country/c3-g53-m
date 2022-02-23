742e188f1c08b60afa13ccb4182af66c3b1a0775
Nicolas Orecchia <nicoaleore@gmail.com>
2022-02-23 16:23:20 -0300

    news-features-advertising

---
+++ src/components/newsFeaturesPublicity/newsFeaturesPublicity.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./newsFeaturesPublicity.css"
+
+export function NewsFeaturesPublicity() {
+    return(
+        <div className='news-features-publicity'>
+            <div className='last-news'>Aca van las noticias</div>
+            <div className="features-publicity">
+                <div className='features'>Aca va el contenido destacado</div>
+                <div className='publicity'>Aca va la publicidad</div>
+            </div>
+        </div>
+    )
+}
