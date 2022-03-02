e3a9d35d0a5fa8eb4db7a6a744e3bfa74e2e2f13
Nicolas Orecchia <nicoaleore@gmail.com>
2022-03-02 15:40:35 -0300

    starting with news cards

---
+++ src/components/newsFeaturesAdvertising/smallNews/smallNews.jsx
@@ -0,0 +1,15 @@
+import "./smallNews.css"
+
+export function SmallNews() {
+    return (
+        <div className="small-news">
+            <div>
+                <img src="./images/Josh-Hart.png"/>
+                <div className="small-news-element">
+                    <p>Billups aplaude a Josh Hart: "Es mi tipo de jugador"</p>
+                    <p>16 de Abril, 22:23</p>
+                </div>
+            </div>
+        </div>
+    )
+}
