4ddde53c6f3457e671d40b4b0ece3dd5a86ec70d
agustin vilarino <lic.avilarino@gmail.com>
2022-03-08 18:32:18 -0300

    api-conect

--- src/components/Match/Match.jsx
+++ src/components/Match/Match.jsx
@@ -1,21 +1,32 @@
-
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./match.css"
 import Wallpaper from "./images/basketball-wallpaper-preview.jpg"
 
 
-
-
 export function Match() {
+
+  const [equipo, setEquipo] = useState(null)
+
+  useEffect(() => {
+    obtenerDatos()
+  }, [])
+
+  const obtenerDatos = async () => {
+    const data = await fetch("")
+    const users = await data.json()
+    setEquipo(users)
+  }
+
+
   return (
     
 
     <div className="container-match">
        <img src={Wallpaper} alt="no-foto" />
-     
+
       <div className="container-text">
-        <p className="match-text">Equipo 1 VS Equipo 2</p> 
-        <p className="match-text">Fecha</p>
+      
+
         <p className="match-text">Stadium</p>
         <p className="coca">Coca - Cola</p> 
         <p className="sprite">Sprite</p> 
