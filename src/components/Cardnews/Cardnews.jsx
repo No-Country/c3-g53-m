01def2b1c8c46cc4fb9fa621de66518eb0196c00
Nicolas Orecchia <nicoaleore@gmail.com>
2022-03-11 01:26:38 -0300

    API card news

--- src/components/Cardnews/Cardnews.jsx
+++ src/components/Cardnews/Cardnews.jsx
@@ -36,69 +36,28 @@ export function Cardnews() {
   }, []);
 
   console.log(news)
-  /*
+  
     return (
     <>
-       <HeaderNews/> 
+      <HeaderNews/> 
       <div className="wrapper">
        <h1>Noticias</h1>
-      <div class="container">
-        <div class="card">
-          <div class="card-header">
-            <img src="https://fotografias.lasexta.com/clipping/cmsimages02/2021/03/08/9E679C81-CECA-471F-9C33-8FFF777CBE59/98.jpg?crop=3005,1691,x0,y0&width=1900&height=1069&optimize=low&format=webply" alt="rover" />
-          </div>    
-            <h4>
-              Stephen Curry arrasa con el Allstar
-            </h4>        
+        <div class="container">
+        {news.map((news, _id) =>(
+          <div class="card" key={_id}>
+            <div class="card-header">
+              <img src={news.imgUrl} alt="rover" />
+            </div>    
+              <h4>
+                {news.title}
+              </h4>
+              <h6>{news.subtitle}</h6>
+              <p>{news.info}</p>   
+          </div>     
+        ))}
+       </div>
         </div>
-
-        <div class="card">
-          <div class="card-header">
-            <img src="https://fotografias.lasexta.com/clipping/cmsimages02/2021/03/08/9E679C81-CECA-471F-9C33-8FFF777CBE59/98.jpg?crop=3005,1691,x0,y0&width=1900&height=1069&optimize=low&format=webply" alt="rover" />
-          </div>    
-            <h4>
-              Stephen Curry arrasa con el Allstar
-            </h4>        
-        </div>
-
-        <div class="card">
-          <div class="card-header">
-            <img src="https://fotografias.lasexta.com/clipping/cmsimages02/2021/03/08/9E679C81-CECA-471F-9C33-8FFF777CBE59/98.jpg?crop=3005,1691,x0,y0&width=1900&height=1069&optimize=low&format=webply" alt="rover" />
-          </div>    
-            <h4>
-              Stephen Curry arrasa con el Allstar
-            </h4>        
-        </div>
-
-        <div class="card">
-          <div class="card-header">
-            <img src="https://fotografias.lasexta.com/clipping/cmsimages02/2021/03/08/9E679C81-CECA-471F-9C33-8FFF777CBE59/98.jpg?crop=3005,1691,x0,y0&width=1900&height=1069&optimize=low&format=webply" alt="rover" />
-          </div>    
-            <h4>
-              Stephen Curry arrasa con el Allstar
-            </h4>        
-        </div>
-
-        <div class="card">
-          <div class="card-header">
-            <img src="https://fotografias.lasexta.com/clipping/cmsimages02/2021/03/08/9E679C81-CECA-471F-9C33-8FFF777CBE59/98.jpg?crop=3005,1691,x0,y0&width=1900&height=1069&optimize=low&format=webply" alt="rover" />
-          </div>    
-            <h4>
-              Stephen Curry arrasa con el Allstar
-            </h4>        
-        </div>
-
-        <div class="card">
-          <div class="card-header">
-            <img src="https://fotografias.lasexta.com/clipping/cmsimages02/2021/03/08/9E679C81-CECA-471F-9C33-8FFF777CBE59/98.jpg?crop=3005,1691,x0,y0&width=1900&height=1069&optimize=low&format=webply" alt="rover" />
-          </div>    
-            <h4>
-              Stephen Curry arrasa con el Allstar
-            </h4>        
-        </div>
-      </div>
-      </div>
       <Footer />
-      </>
-    )*/
+    </>
+  )
 }
