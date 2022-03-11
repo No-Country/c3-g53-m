88dca1019802183ff5a05ca7ac156064be1e78cf
oliveravictor <ovictoriox@gmail.com>
2022-03-11 00:51:49 -0300

    new layout in route

--- src/components/Cardnews/Cardnews.jsx
+++ src/components/Cardnews/Cardnews.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Cardnews.css";
 import HeaderNews from "../HeaderNews/HeaderNews";
+import Footer from "../Footer/Footer"
 
 export function Cardnews() {
     return (
-      <>
+    <>
        <HeaderNews/> 
+      <div className="wrapper">
        <h1>Noticias</h1>
       <div class="container">
         <div class="card">
@@ -43,8 +45,27 @@ export function Cardnews() {
               Stephen Curry arrasa con el Allstar
             </h4>        
         </div>
-            
+
+        <div class="card">
+          <div class="card-header">
+            <img src="https://fotografias.lasexta.com/clipping/cmsimages02/2021/03/08/9E679C81-CECA-471F-9C33-8FFF777CBE59/98.jpg?crop=3005,1691,x0,y0&width=1900&height=1069&optimize=low&format=webply" alt="rover" />
+          </div>    
+            <h4>
+              Stephen Curry arrasa con el Allstar
+            </h4>        
+        </div>
+
+        <div class="card">
+          <div class="card-header">
+            <img src="https://fotografias.lasexta.com/clipping/cmsimages02/2021/03/08/9E679C81-CECA-471F-9C33-8FFF777CBE59/98.jpg?crop=3005,1691,x0,y0&width=1900&height=1069&optimize=low&format=webply" alt="rover" />
+          </div>    
+            <h4>
+              Stephen Curry arrasa con el Allstar
+            </h4>        
+        </div>
       </div>
+      </div>
+      <Footer />
       </>
     )
 }
